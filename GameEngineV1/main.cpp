66334a6c9d1d1333167ca260eb977bd7475e19bd
Daniel Harrison <harrison92d+orangudan@gmail.com>
2016-11-18 21:06:52 +0000

    Created a GL window

--- GameEngineV1/main.cpp
+++ GameEngineV1/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World" << std::endl;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "Hello GL", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+
 	return EXIT_SUCCESS;
 }
